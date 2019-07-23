dfd811dd3d7e16cc7c91fa423bee4f08d5a50e8e
Jeremy Burman <jburman@gmail.com>
2019-07-22 21:55:02 -0400

    Change behavior of "value cap" processor. Create interface for counter type and allow alternate implementation to be supplied to monitor class.

--- src/limitbreak/LimitBreak.fs
+++ src/limitbreak/LimitBreak.fs
@@ -7,61 +7,74 @@ type MonitorStatus =
 | Ok
 | Limited
 
+type IEventCounter =
+    interface
+    abstract member EventId:unit -> int
+    abstract member Increment:unit -> unit
+    abstract member DecrementBy:int -> unit
+    abstract member Reset:int -> unit
+    abstract member Observed:unit -> int
+end
+
 /// Encapsulates a thread safe counter associated with an eventId.
 type EventCounter(eventId: int) =
     let observed = ref 0;
     let limit = System.Int32.MaxValue - 10_000;
 
-    member this.eventId = eventId
+    interface IEventCounter with
+        member this.EventId() = eventId
 
-    member this.Increment =
-        let newValue = Interlocked.Increment observed
-        if newValue > limit then
-            Interlocked.CompareExchange (observed, limit, newValue) |> ignore
+        member this.Increment() =
+            let newValue = Interlocked.Increment observed
+            if newValue > limit then
+                Interlocked.CompareExchange (observed, limit, newValue) |> ignore
 
-    member this.DecrementBy (amount: int) =
-        let rec tryDecrement decrAmount =
-            let currentObserved = observed.contents
-            let newValue = Math.Min(Math.Max(0, currentObserved - decrAmount), limit)
-            match Interlocked.CompareExchange (observed, newValue, currentObserved) with
-            | returnValue when returnValue = currentObserved -> newValue
-            | _ -> tryDecrement decrAmount
-        tryDecrement amount |> ignore
+        member this.DecrementBy (amount: int) =
+            let rec tryDecrement decrAmount =
+                let currentObserved = observed.contents
+                let newValue = Math.Min(Math.Max(0, currentObserved - decrAmount), limit)
+                match Interlocked.CompareExchange (observed, newValue, currentObserved) with
+                | returnValue when returnValue = currentObserved -> newValue
+                | _ -> tryDecrement decrAmount
+            tryDecrement amount |> ignore
 
-    member this.Reset value =
-        Interlocked.Exchange (observed, Math.Max(0, Math.Min(value, limit))) |> ignore
+        member this.Reset value =
+            Interlocked.Exchange (observed, Math.Max(0, Math.Min(value, limit))) |> ignore
 
-    member this.Observed =
-        observed.contents
+        member this.Observed() =
+            observed.contents
 
 /// Encapsulates an EventCounter and a set of "event processor" functions that operate on the counter 
 /// in order. Each process has the opportunity to read the counter and modify it and dtermine if the 
 /// event should be "limited" or not.
-type EventMonitor (eventId: int, eventProcessors: (EventCounter -> MonitorStatus -> MonitorStatus) list) =
-    let counter = new EventCounter(eventId)
+type EventMonitor (counter: IEventCounter, eventProcessors: (IEventCounter -> MonitorStatus -> MonitorStatus) list) =
+    
+    new(eventId: int, eventProcessors: (IEventCounter -> MonitorStatus -> MonitorStatus) list) = 
+        EventMonitor(new EventCounter(eventId) :> IEventCounter, eventProcessors)
+    
+    member private this.processors = eventProcessors
 
-    member this.processors = eventProcessors
+    member this.EventId = 
+        counter.EventId()
 
-    member this.eventId = counter.eventId
-
-    member this.GetStatus =
+    member this.GetStatus() =
         let status = 
             List.fold (fun (status: MonitorStatus) proc -> proc counter status) MonitorStatus.Ok this.processors
-        let observed = counter.Observed
+        let observed = counter.Observed()
         status, observed
 
-    member this.Increment =
-        counter.Increment
+    member this.Increment() =
+        counter.Increment()
 
-    member this.Reset =
-        counter.Reset
+    member this.Reset() =
+        counter.Reset(0)
 
 module limitbreak = 
   
     /// Returns a MonitorStatus of limited when counter equals or exceeds a given value.
     let createThreshold limit =
-        fun (eventCounter: EventCounter) (monitorStatus: MonitorStatus) ->
-            if eventCounter.Observed >= limit then
+        fun (eventCounter: IEventCounter) (monitorStatus: MonitorStatus) ->
+            if eventCounter.Observed() >= limit then
                 MonitorStatus.Limited
             else
                 monitorStatus
@@ -72,7 +85,7 @@ module limitbreak =
         let decayPerMillis = decayPerSecond / 1000.0
         let syncLock = Object
         let mutable lastRun = DateTime.Now
-        fun (eventCounter: EventCounter) (monitorStatus: MonitorStatus) ->
+        fun (eventCounter: IEventCounter) (monitorStatus: MonitorStatus) ->
             try
                 Monitor.Enter syncLock
 
@@ -92,13 +105,9 @@ module limitbreak =
             monitorStatus
     
     // Applies a value cap (maximum value) on the event counter.
-    // Returns a MonitorStatus of limited once the event counter reches the cap value.
+    // Does not change monitor status
     let createValueCap cap =
-        fun (eventCounter: EventCounter) (monitorStatus: MonitorStatus) ->
-            if eventCounter.Observed = cap then
-                MonitorStatus.Limited
-            elif eventCounter.Observed > cap then
+        fun (eventCounter: IEventCounter) (monitorStatus: MonitorStatus) ->
+            if eventCounter.Observed() > cap then
                 eventCounter.Reset cap
-                MonitorStatus.Limited
-            else
-                monitorStatus
+            monitorStatus
--- tests/limitbreak.Tests/Tests.fs
+++ tests/limitbreak.Tests/Tests.fs
@@ -18,109 +18,127 @@ module Tests =
             testCase "construct with empty list" <| fun () ->
                 let eventMon = new EventMonitor(100, [])
 
-                Expect.equal 100 eventMon.eventId "Expected eventId to be 100"
+                Expect.equal 100 eventMon.EventId "Expected eventId to be 100"
 
-                let (status, count) = eventMon.GetStatus
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 0 "Expected count of 0"
                 Expect.equal status MonitorStatus.Ok "Expected Status Ok"
 
-                eventMon.Increment
-                let (status, count) = eventMon.GetStatus
+                eventMon.Increment()
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 1 "Expected count of 1"
+                Expect.equal status MonitorStatus.Ok "Expected Status Ok"
+
+            testCase "reset" <| fun () ->
+                let eventMon = new EventMonitor(100, [])
+
+                Expect.equal 100 eventMon.EventId "Expected eventId to be 100"
+
+                eventMon.Increment()
+                eventMon.Increment()
+
+                let (status, count) = eventMon.GetStatus()
+                Expect.equal count 2 "Expected count of 2"
+                Expect.equal status MonitorStatus.Ok "Expected Status Ok"
+                
+                eventMon.Reset()
+
+                let (status, count) = eventMon.GetStatus()
+                Expect.equal count 0 "Expected count of 0"
                 Expect.equal status MonitorStatus.Ok "Expected Status Ok"
                 
 
             testCase "value cap" <| fun () ->
                 let eventMon = new EventMonitor(1, [limitbreak.createValueCap 100])
                 
-                let (status, count) = eventMon.GetStatus
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 0 "Expected count of 0"
                 Expect.equal status MonitorStatus.Ok "Expected Status Ok"
 
                 for i in 1 .. 99 do
-                    eventMon.Increment
-                let (status, count) = eventMon.GetStatus
+                    eventMon.Increment()
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 99 "Expected count of 99"
                 Expect.equal status MonitorStatus.Ok "Expected Status Ok"
 
-                eventMon.Increment
-                let (status, count) = eventMon.GetStatus
+                eventMon.Increment()
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 100 "Expected count of 100"
-                Expect.equal status MonitorStatus.Limited "Expected Status Limited"
+                Expect.equal status MonitorStatus.Ok "Expected Status Ok"
 
-                eventMon.Increment
-                let (status, count) = eventMon.GetStatus
+                eventMon.Increment()
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 100 "Expected count of 100"
-                Expect.equal status MonitorStatus.Limited "Expected Status Limited"
+                Expect.equal status MonitorStatus.Ok "Expected Status Ok"
 
             testCase "threshold" <| fun () ->
                 let eventMon = new EventMonitor(1, [limitbreak.createThreshold 5])
                 
-                let (status, count) = eventMon.GetStatus
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 0 "Expected count of 0"
                 Expect.equal status MonitorStatus.Ok "Expected Status Ok"
 
                 for i in 1 .. 4 do
-                    eventMon.Increment
-                let (status, count) = eventMon.GetStatus
+                    eventMon.Increment()
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 4 "Expected count of 4"
                 Expect.equal status MonitorStatus.Ok "Expected Status Ok"
 
-                eventMon.Increment
-                let (status, count) = eventMon.GetStatus
+                eventMon.Increment()
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 5 "Expected count of 5"
                 Expect.equal status MonitorStatus.Limited "Expected Status Limited"
 
-                eventMon.Increment
-                let (status, count) = eventMon.GetStatus
+                eventMon.Increment()
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 6 "Expected count of 6"
                 Expect.equal status MonitorStatus.Limited "Expected Status Limited"
 
             testCase "decay rate" <| fun () ->
                 let eventMon = new EventMonitor(1, [limitbreak.createDecayRate 5.0])
                 
-                let (status, count) = eventMon.GetStatus
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 0 "Expected count of 0"
                 Expect.equal status MonitorStatus.Ok "Expected Status Ok"
 
                 for i in 1 .. 20 do
-                    eventMon.Increment
-                let (status, count) = eventMon.GetStatus
+                    eventMon.Increment()
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 20 "Expected count of 20"
                 Expect.equal status MonitorStatus.Ok "Expected Status Ok"
 
                 Thread.Sleep 2000
 
-                let (status, count) = eventMon.GetStatus
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 10 "Expected count of 10"
                 Expect.equal status MonitorStatus.Ok "Expected Status Ok"
 
                 Thread.Sleep 1000
 
-                let (status, count) = eventMon.GetStatus
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 5 "Expected count of 5"
                 Expect.equal status MonitorStatus.Ok "Expected Status Ok"
 
             testCase "leaky bucket" <| fun () ->
-                let eventMon = new EventMonitor(1, [limitbreak.createDecayRate 5.5; limitbreak.createValueCap 100; limitbreak.createThreshold 90])
+                let eventMon = new EventMonitor(1, [limitbreak.createValueCap 100; limitbreak.createDecayRate 5.5; limitbreak.createThreshold 90])
                     
-                let (status, count) = eventMon.GetStatus
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 0 "Expected count of 0"
                 Expect.equal status MonitorStatus.Ok "Expected Status Ok"
 
                 for i in 1 .. 150 do
-                    eventMon.Increment
-                let (status, count) = eventMon.GetStatus
+                    eventMon.Increment()
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 100 "Expected count of 100"
                 Expect.equal status MonitorStatus.Limited "Expected Status Limited"
 
                 Thread.Sleep 2000
-                let (status, count) = eventMon.GetStatus
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 89 "Expected count of 89"
                 Expect.equal status MonitorStatus.Ok "Expected Status Ok"
 
-                eventMon.Increment
-                let (status, count) = eventMon.GetStatus
+                eventMon.Increment()
+                let (status, count) = eventMon.GetStatus()
                 Expect.equal count 90 "Expected count of 90"
                 Expect.equal status MonitorStatus.Limited "Expected Status Limited"
         ]
